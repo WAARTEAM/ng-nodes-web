b8fb6c64e9f90dd93d83e511b123468004e46801
BigBBaker <abobkerelaghel@gmail.com>
2020-01-16 19:05:59 +0100

    *

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -10,6 +10,7 @@ export class AuthService {
   user: any;
   constructor(private http: HttpClient) {}
   registerUser(user) {
+
     return this.http.post("http://waar-nodes.herokuapp.com/api/users", user);
     // .subscribe(data => console.log(data));
     // .pipe(map((response: any) => response.json()))
