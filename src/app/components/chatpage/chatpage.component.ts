f583091d3973f336a18a0647fa37800b135431d8
rue-elwahishi <rubawahishi@gmail.com>
2020-01-17 22:59:49 +0100

    did the messages,added getMessages,Post Messages on the front-end

--- src/app/components/chatpage/chatpage.component.ts
+++ src/app/components/chatpage/chatpage.component.ts
@@ -8,46 +8,74 @@ import { HttpService } from "src/app/services/http/http.service";
 })
 export class ChatpageComponent implements OnInit {
   constructor(private http: HttpService) {}
+  currentReceiver: any;
+  currentUser: any;
+  ngOnInit() {
+    this.currentUser = localStorage.getItem("username");
+    this.http.get(`/messages/latest`).subscribe(data => {
+      this.latest = data;
+      this.currentReceiver =
+        this.latest[0].sender.username === this.currentUser
+          ? this.latest[0].receiver._id
+          : this.latest[0].sender._id;
+      this.http
+        .get(`/users/${this.currentReceiver}/messages`)
+        .subscribe(messages => {
+          this.messages = messages;
+        });
+    });
+  }
+  content: String;
 
-  ngOnInit() {}
-  messages = [
-    {
-      username: "Mathew MacConhey",
-      content: "all right all right all right ",
-      sent: "5 min ago",
-      senderPhoto: ""
-    },
-    {
-      username: "Samuel L Jakson",
-      content: "shut up  Mothe*****************er ",
-      sent: "5 min ago",
-      senderPhoto: ""
-    },
+  sendMessage() {
+    this.http
+      .post(`/users/${this.currentReceiver}/messages`, {
+        content: this.content
+      })
+      .subscribe(data => {
+        //logic of adding the message as a template to the chat
+        this.messages.push(data);
+        console.log(this.messages);
+      });
+  }
 
-    {
-      username: "Kevin Hart",
-      content: "Oh no, no no no  ",
-      sent: "5 min ago",
-      senderPhoto: ""
-    },
+  messages: any;
+  // {
+  //   username: "Mathew MacConhey",
+  //   content: "all right all right all right ",
+  //   sent: "5 min ago",
+  //   senderPhoto: ""
+  // },
+  // {
+  //   username: "Samuel L Jakson",
+  //   content: "shut up  Mothe*****************er ",
+  //   sent: "5 min ago",
+  //   senderPhoto: ""
+  // },
+  // {
+  //   username: "Kevin Hart",
+  //   content: "Oh no, no no no  ",
+  //   sent: "5 min ago",
+  //   senderPhoto: ""
+  // },
+  // {
+  //   username: "Dave Chapelle",
+  //   content: "Yeaah I said it ",
+  //   sent: "5 min ago",
+  //   senderPhoto: ""
+  // },
+  // {
+  //   username: "Arnold SChwarziniger",
+  //   content: "I'm Back",
+  //   sent: "5 min ago",
+  //   senderPhoto: ""
+  // },
+  // {
+  //   username: "Amelia Clark",
+  //   content: "Dracarys ",
+  //   sent: "5 min ago",
+  //   senderPhoto: ""
+  // }
 
-    {
-      username: "Dave Chapelle",
-      content: "Yeaah I said it ",
-      sent: "5 min ago",
-      senderPhoto: ""
-    },
-    {
-      username: "Arnold SChwarziniger",
-      content: "I'm Back",
-      sent: "5 min ago",
-      senderPhoto: ""
-    },
-    {
-      username: "Amelia Clark",
-      content: "Dracarys ",
-      sent: "5 min ago",
-      senderPhoto: ""
-    }
-  ];
+  latest: any;
 }
