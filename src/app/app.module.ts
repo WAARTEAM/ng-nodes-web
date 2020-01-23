f0e11705882f919733439cfbab85cd1f7cb4b254
Abobker Elaghel <57757187+AbobkerElaghel@users.noreply.github.com>
2020-01-23 14:41:45 +0100

    FINAL COMMIT

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { MDBBootstrapModule } from "angular-bootstrap-md";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { FormsModule } from "@angular/forms";
-import { SignupComponent } from "./signup/signup.component";
-import { LandingComponent } from "./components/landing/landing.component";
-import { NavbarComponent } from "./components/navbar/navbar.component";
-import { LoginComponent } from "./components/login/login.component";
-import { FlashMessagesModule } from "angular2-flash-messages";
-import { AuthService } from "./services/auth.service";
-import { ValidateService } from "./services/validate.service";
-import { ProfileComponent } from "./components/profile/profile.component";
-import { ChatpageComponent } from "./components/chatpage/chatpage.component";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { SignupComponent } from './signup/signup.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { FlashMessagesModule } from 'angular2-flash-messages';
+import { AuthService } from './services/auth.service';
+import { ValidateService } from './services/validate.service';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ChatpageComponent } from './components/chatpage/chatpage.component';
+import { HttpClientModule } from '@angular/common/http';
 import { AuthGuard } from './guards/auth.guard';
-import {NotAuthGuard} from './guards/notauth.guard'
+import {NotAuthGuard} from './guards/notauth.guard';
+import { SettingsComponent } from './components/settings/settings.component';
+import { SearchAreaComponent } from './components/search-area/search-area.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,9 @@ import {NotAuthGuard} from './guards/notauth.guard'
     LoginComponent,
     SignupComponent,
     ProfileComponent,
-    ChatpageComponent
+    ChatpageComponent,
+    SettingsComponent,
+    SearchAreaComponent
   ],
   imports: [
     BrowserModule,
@@ -35,9 +39,9 @@ import {NotAuthGuard} from './guards/notauth.guard'
     FlashMessagesModule.forRoot(),
     HttpClientModule
   ],
-  providers: [ValidateService, AuthService,AuthGuard,NotAuthGuard],
+  providers: [ValidateService, AuthService, AuthGuard, NotAuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
-//back 
+ // back
 
